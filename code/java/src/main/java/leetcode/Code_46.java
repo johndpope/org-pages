905e34ed62ef14f109962a3090ef458c80d08115
Nanue1 <manue1@foxmail.com>
2018-11-15 19:32:02 +0800

    20181115

--- code/java/src/main/java/leetcode/Code_46.java
+++ code/java/src/main/java/leetcode/Code_46.java
@@ -5,26 +5,49 @@ import java.util.ArrayList;
 
 public class Code_46 {
     /**
-     * @param nums 给一个不重复整数集，输出所有可能组合
+     * @param nums
      * @return
      */
     public List<List<Integer>> permute(int[] nums) {
-        ArrayList<List<Integer>> res = new ArrayList<List<Integer>>();
 
-        return null;
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        backtrack(list, nums, 0);
+        return list;
+
     }
 
-    private void permutation(int[] nums, int index, int length) {
-        while (index < length - 1) {
+    public List<List<Integer>> backtrack(List<List<Integer>> list, int[] nums, int start) {
 
+        if (start >= nums.length) {
+            List<Integer> tempList = convertArrayToList(nums);
+            list.add(tempList);
         }
 
+        for (int i = start; i < nums.length; i++) {
+            swap(nums, start, i);
+            backtrack(list, nums, start + 1);
+            swap(nums, start, i);
+        }
+
+        return list;
 
     }
 
-    private void swap(int[] nums, int i, int j) {
-        int t = nums[i];
-        nums[i] = nums[j];
-        nums[j] = t;
+    private List<Integer> convertArrayToList(int[] nums) {
+
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            list.add(nums[i]);
+        }
+
+        return list;
+
     }
+
+    private void swap(int[] num, int i, int j) {
+        int temp = num[i];
+        num[i] = num[j];
+        num[j] = temp;
+    }
+
 }
